add93982a3affe2efbb6bc744ab06184894975aa
idrak07 <idrak.10225@gmail.com>
2021-04-13 12:16:41 +0600

    Project Initialized

---
+++ src/test/java/com/myblog/intern/BlogSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myblog.intern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
