e7adcfff8ff870c348e50667c89a8c3fc5930911
Tisa0123 <zferdous83@yahoo.com>
2021-05-04 16:05:51 +0600

    solving id problem with jwt

--- src/main/java/com/myblog/intern/controller/EditProfileController.java
+++ src/main/java/com/myblog/intern/controller/EditProfileController.java
@@ -1,11 +1,18 @@
 package com.myblog.intern.controller;
 
+import com.myblog.intern.model.User;
 import com.myblog.intern.model.UserDetails;
 import com.myblog.intern.service.EditProfileService;
+import com.myblog.intern.service.JwtService;
+import com.myblog.intern.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Optional;
 
 
 @RestController
@@ -13,12 +20,16 @@ import org.springframework.web.bind.annotation.*;
 public class EditProfileController {
     @Autowired
     private EditProfileService editProfileService;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    JwtService jwtService;
 
     public EditProfileController(EditProfileService editProfileService) {
         this.editProfileService = editProfileService;
     }
 
-    @GetMapping(value = "/getById")
+    @GetMapping(value = "/getByUsersId")
     public ResponseEntity<UserDetails> getUsersById(@RequestParam int id)  throws RuntimeException {
         UserDetails userDetails=null;
         try {
@@ -31,10 +42,46 @@ public class EditProfileController {
         return new ResponseEntity<UserDetails> (userDetails, HttpStatus.OK);
     }
 
-    @PostMapping("/Update")
-    public ResponseEntity<String>addOrUpdate(@RequestBody UserDetails userDetails)  throws RuntimeException {
+    @GetMapping(value = "/getById")
+    public ResponseEntity<UserDetails> getUsersById(HttpServletRequest request)  throws RuntimeException {
+        String username= jwtService.extractUserName(jwtService.parseToken(request));
+        Integer userIdRequest= userService.getUserIdByUserName(username);
+        UserDetails userDetails=null;
         try {
-            editProfileService.saveUpdateProfile(userDetails);
+            userDetails=editProfileService.getUsersByUserId(userIdRequest);
+
+        }catch (Exception ex){
+            ex.getMessage();
+            return new ResponseEntity<> (null, HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<UserDetails> (userDetails, HttpStatus.OK);
+    }
+    @GetMapping(value = "/getAllUserDetails")
+    public ResponseEntity<List<UserDetails>> getAllUserProfiles()  throws RuntimeException {
+        List<UserDetails> userDetails=null;
+        try {
+            userDetails=editProfileService.getAll();
+
+        }catch (Exception ex){
+            ex.getMessage();
+            return new ResponseEntity<> (null, HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<> (userDetails, HttpStatus.OK);
+    }
+
+    @PostMapping("/Update")
+    public ResponseEntity<String>addOrUpdate(@RequestBody UserDetails userDetails,HttpServletRequest request)  throws RuntimeException {
+        String username= jwtService.extractUserName(jwtService.parseToken(request));
+        Integer userIdRequest= userService.getUserIdByUserName(username);
+       // userDetails.setUserId(userIdRequest);
+        if(userIdRequest!=userDetails.getUserId())  return new ResponseEntity<String> ("Not a valid user", HttpStatus.INTERNAL_SERVER_ERROR);
+        try {
+            if(editProfileService.saveUpdateProfile(userDetails)!=null){
+                Optional<User> user=userService.findById(userDetails.getUserId());
+                user.get().setRole(userDetails.getRole());
+                user.get().setEmail(userDetails.getEmail());
+                userService.save(user.get());
+            }else  return new ResponseEntity<String> ("Error occur", HttpStatus.INTERNAL_SERVER_ERROR);
 
         }catch (Exception ex){
             ex.getMessage();
@@ -43,9 +90,12 @@ public class EditProfileController {
         return new ResponseEntity<String> ("Successfully Updated User Profile", HttpStatus.OK);
     }
     @PostMapping("/delete")
-    public ResponseEntity<String>deleteById(@RequestBody UserDetails userDetails)  throws RuntimeException {
+    public ResponseEntity<String>deleteById(@RequestBody UserDetails userDetails,HttpServletRequest request)  throws RuntimeException {
+        String username= jwtService.extractUserName(jwtService.parseToken(request));
+        Integer userIdRequest= userService.getUserIdByUserName(username);
+        if(userIdRequest!=userDetails.getUserId())  return new ResponseEntity<String> ("Not a valid user", HttpStatus.OK);
         try {
-            editProfileService.DeleteProfile(userDetails);
+            editProfileService.DeleteProfile(userDetails,request);
 
         }catch (Exception ex){
             ex.getMessage();
--- src/main/java/com/myblog/intern/service/EditProfileService.java
+++ src/main/java/com/myblog/intern/service/EditProfileService.java
@@ -1,16 +1,18 @@
 package com.myblog.intern.service;
 
-import com.myblog.intern.model.User;
 import com.myblog.intern.model.UserDetails;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 
 public interface EditProfileService {
-    public UserDetails getUsersById(int userId);
+    public UserDetails getUsersById(int id);
+    public UserDetails getUsersByUserId(int userId);
     public UserDetails saveUpdateProfile(UserDetails userDetails);
-    public boolean DeleteProfile(UserDetails userDetails);
+    public boolean DeleteProfile(UserDetails userDetails, HttpServletRequest request);
 
     List<UserDetails> getAllByRole(String role);
     UserDetails getUserDetailsByUserID(int id);
+    List<UserDetails>getAll();
 }
